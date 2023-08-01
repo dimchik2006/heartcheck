3a7fb19d435a704a313978f1ba58ad1340d54c20
Ivan-Khlebus <vanya061205@tut.by>
2023-08-01 11:09:52 +0300

    update

--- final_win.py
+++ final_win.py
@@ -1,1 +1,4 @@
-# напиши здесь код третьего экрана приложения
+e3_text1 = QLabel('Индекс Руфье:')
+e3_text2 = QLabel('Ваш уровень здоровья:')
+e3_text1.setFont(QFont('Times', 14, QFont.Bold))
+e3_text2.setFont(QFont('Times', 14, QFont.Bold))
